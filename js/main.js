c3cf7714787d786d0c9cebd501b1b4e57178cf48
dasturcom <abdujabborshavkatjonov@gmail.com>
2022-08-31 00:23:38 +0500

    JavaScript

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var elSiteForm = document.querySelector(".site-form");
+var elSiteInput = document.querySelector("#current-money");
+var elBtn = document.querySelector("#btn-js");
+var elSelect = document.querySelector("#valyuta-select");
+var elMessage = document.querySelector(".message");
+
+
+elSiteForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    var inputValue = parseFloat(elSiteInput.value, 10);
+
+    elMessage.textContent = inputValue * elSelect.value, sum;
+})
